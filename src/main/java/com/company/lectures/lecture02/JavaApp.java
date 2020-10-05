319c7975e7512bd37c16d0ea602e7e78fef7ed9a
ttyagur <tyagur.tatiana@gmail.com>
2020-10-05 10:08:39 +0300

    Lecture 2 classes.

--- src/main/java/com/company/lectures/lecture02/JavaApp.java
+++ src/main/java/com/company/lectures/lecture02/JavaApp.java
@@ -4,6 +4,7 @@ public class JavaApp {
     public static void main(String[] args) {
         System.out.println(System.getenv("JAVA_HOME"));
         System.out.println(System.getenv("testBrowser"));
+        System.out.println(System.getenv("M2_HOME"));
 
     }
 }
