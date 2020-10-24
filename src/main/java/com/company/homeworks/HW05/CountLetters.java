8cc0e6c2a0d71c384f25e9b7bd22c450ed118a20
ttyagur <tyagur.tatiana@gmail.com>
2020-10-24 18:53:51 +0300

    Homework 05. Added changes

--- src/main/java/com/company/homeworks/HW05/CountLetters.java
+++ src/main/java/com/company/homeworks/HW05/CountLetters.java
@@ -2,16 +2,29 @@ package com.company.homeworks.HW05;
 
 public class CountLetters {
     public static void main(String[] args) {
-        String str="В древнем городе Лондоне , в один осенний день второй половине шестнадцатого столетия , в бедной семье по фамилии Канти родился ребенок – мальчик .";
-        String [] res= str.split(" ");
-        char [] ch;
-        int cnt=0;
-        for (int i = 0; i < res.length; i++) {
-            ch = res[i].toCharArray();
-            if(ch.length==1){
-                cnt++;
+        String str = "Hello world!";
+        char[] charArr = new char[str.length()];
+        int cnt = 0;
+        for (int i = 0; i <str.length() ; i++) {
+            boolean fl=false;
+            for (int k = 0; k < charArr.length; k++) {
+                if(str.charAt(i)==charArr[k]){
+                    fl=true;
+                    break;
+                }
             }
+            if(fl){
+                continue;
+            }
+            for (int j = 0; j < str.length(); j++) {
+                if(str.charAt(i)==str.charAt(j)){
+                        cnt++;
+                }
+            }
+            charArr[i]=str.charAt(i);
+            System.out.println("Number of "+charArr[i]+" symbols = "+cnt);
+            cnt=0;
         }
-        System.out.println("Number of single characters: "+cnt);
+
     }
 }
