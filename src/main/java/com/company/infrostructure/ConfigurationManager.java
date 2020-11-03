1c2bff472ce13c4fd583061aac953b0d4e68ea78
ttyagur <tyagur.tatiana@gmail.com>
2020-11-03 17:53:05 +0200

    Homework 08

--- src/main/java/com/company/infrostructure/ConfigurationManager.java
+++ src/main/java/com/company/infrostructure/ConfigurationManager.java
@@ -1,7 +1,30 @@
 package com.company.infrostructure;
 // Получение значений переменных из окружения
+
 public class ConfigurationManager {
-    public static String getEnvironmentVariableOrDefault(String envVar, String defaultValue) {
+    private String testBrowser="";
+    private String testEnv="";
+    private static ConfigurationManager INSTANCE;
+
+    private ConfigurationManager(){
+
+    }
+
+    public static ConfigurationManager getInstance(){
+        if(INSTANCE==null){
+            INSTANCE= new ConfigurationManager();
+        }
+        return INSTANCE;
+    }
+    public String getTestBrowser(){
+       return testBrowser;
+    }
+
+    public String getTestEnv(){
+        return testEnv;
+    }
+
+    private String getEnvironmentVariableOrDefault(String envVar, String defaultValue) {
        return System.getenv(envVar) !=null&& !System.getenv(envVar).isBlank() ?
                System.getenv(envVar): defaultValue;
     }
